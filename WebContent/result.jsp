cd78920cd24c17d550f0de39f2d8543418c40769
prajwoldangol <webdevprajwol@gmail.com>
2021-04-22 20:25:53 -0700

    Changes in view

--- WebContent/result.jsp
+++ WebContent/result.jsp
@@ -4,15 +4,64 @@
 <html>
 <head>
 <meta charset="ISO-8859-1">
-<title>Insert title here</title>
+<link
+	href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;300;500;600;700;900&display=swap"
+	rel="stylesheet">
+<link href="style.css" rel="stylesheet" >
+<title>Student List</title>
 </head>
 <body>
 
-<section class="results">
-	<table border="1">
-		
-	</table>
-</section>
+	<section class="results">
+	<div class="tables">
+	<header>
+		<h1> List Of Registered Students </h1>
+	</header>
+		<table>
+			<tr>
+				<th>Student Id</th>
+				<th>First Name</th>
+				<th>Last Name</th>
+				<th>Middle Name</th>
+				<th>Phone</th>
+				<th>Email</th>
+				<th>Gender</th>
+				<th>Date OF Birth</th>
+				<th>Address 1</th>
+				<th>Address 2</th>
+				<th>State</th>
+				<th>City</th>
+				<th>Zip / Postal code</th>
+				<th>Actions</th>
+			</tr>
+			<c:forEach var="std" items="${students}">
+				<tr>
+					<td><c:out value="${std.id}" /></td>
+					<td><c:out value="${std.fname}" /></td>
+					<td><c:out value="${std.lname}" /></td>
+					<td><c:out value="${std.mname}" /></td>
+					<td><c:out value="${std.phone}" /></td>
+					<td><c:out value="${std.email}" /></td>
+					<td><c:out value="${std.gender}" /></td>
+					<td><c:out value="${std.dob}" /></td>
+					<td><c:out value="${std.adddress1}" /></td>
+					<td><c:out value="${std.address2}" /></td>
+					<td><c:out value="${std.state}" /></td>
+					<td><c:out value="${std.city}" /></td>
+					<td><c:out value="${std.zip}" /></td>
+					<td>
+						<a href="<c:out value="${url}" />/edit?sid=<c:out value="${std.id}" />"> Edit </a>
+						<a href="<c:out value="${url}" />/Delete?sid=<c:out value="${std.id}" />" onclick="return confirm('Are You Sure to delete it ?');"> Delete </a>
+					</td>
+
+				</tr>
+				</c:forEach>
+		</table>
+		<div class="submitbtn">
+		<a href="<c:out value="${url}" />/InsertData"> Register New Student</a>
+		</div>
+		</div>
+	</section>
 
 </body>
 </html>
--- src/ViewData.java
+++ src/ViewData.java
@@ -3,8 +3,11 @@ import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.util.Arrays;
+import java.util.ArrayList;
+//import java.util.Arrays;
+import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -19,58 +22,35 @@ import javax.servlet.http.HttpServletResponse;
 public class ViewData extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, 
+	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response)
 					throws ServletException, IOException
 	{
 		try {
 			PrintWriter out = response.getWriter();
-			
+
 			Connection con = DBConnection.initializeDatabase();
 			PreparedStatement ps = con.prepareStatement("SELECT * FROM std_registration");
 			ResultSet rs   =   ps.executeQuery();
-			out.print("<html><head><title>Result</title></head><body><h1>Registered Students</h1><table border=\"1\">");
-			out.print("<th>Student Id</th>");
-			out.print("<th>First Name</th>");
-			out.print("<th>Last Name</th>");
-			out.print("<th>Middle Name</th>");
-			out.print("<th>Phone</th>");
-			out.print("<th>Email</th>");
-			out.print("<th>Gender</th>");
-			out.print("<th>Date OF Birth</th>");
-			out.print("<th>Address 1</th>");
-			out.print("<th>Address 2</th>");
-			out.print("<th>State</th>");
-			out.print("<th>City</th>");
-			out.print("<th>Zip / Postal code</th>");
-			out.print("<th>Actions</th>");
+			String domain  = InsertData.getDomain(request);
 
+			List<RegisterStudent> students = new ArrayList<>();
 			while(rs.next())
 			{
-				out.print("<tr>");
-				out.println("<td>"+rs.getString(1)+"</td>"); 
-				out.println("<td>"+rs.getString(2)+"</td>"); 
-				out.println("<td>"+rs.getString(3)+"</td>"); 
-				out.println("<td>"+rs.getString(4)+"</td>"); 
-				out.println("<td>"+rs.getString(5)+"</td>"); 
-				out.println("<td>"+rs.getString(6)+"</td>"); 
-				out.println("<td>"+rs.getString(7)+"</td>"); 
-				out.println("<td>"+rs.getString(8)+"</td>"); 
-				out.println("<td>"+rs.getString(9)+"</td>"); 
-				out.println("<td>"+rs.getString(10)+"</td>"); 
-				out.println("<td>"+rs.getString(11)+"</td>"); 
-				out.println("<td>"+rs.getString(12)+"</td>"); 
-				out.println("<td>"+rs.getString(13)+"</td>");
-				out.print("<td><a href=\""+InsertData.getDomain(request)+"/Delete?sid="+rs.getString(1)+"\" onclick=\"return confirm('Are you Sure to Delete the Record ?');\"> Delete </a>"+
-						"&nbsp;&nbsp;&nbsp;&nbsp;"
-						+ "<a href=\""+InsertData.getDomain(request)+"/edit?sid="+rs.getString(1)+"\" > Update </a></td>");
-			
-				out.println("</tr>");
+				
+				students.add(new RegisterStudent(rs.getInt("std_id"), rs.getString("std_fname"),rs.getString("std_mname"),rs.getString("std_lname")
+						,rs.getString("std_gender"),rs.getString("std_phone"),rs.getString("std_email"),rs.getString("std_dob")
+						,rs.getString("std_address1"),rs.getString("std_address2"),rs.getString("std_state"),rs.getString("std_city"),rs.getString("std_zip")));
+
 
 			}
-			out.print("</table><br/><a href=\""+InsertData.getDomain(request)+"/InsertData\"> Register New Student </a>");
-			out.print("</body></html>");
-
+//			out.print("acv");
+//			out.print(students);
+			RequestDispatcher res	= request.getRequestDispatcher("/result.jsp") ;
+			request.setAttribute("students", students);
+			request.setAttribute("url", domain);
+			res.forward(request,response);
+	
 			ps.close();
 
 
@@ -78,8 +58,9 @@ public class ViewData extends HttpServlet {
 		catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-	}
 
+	}
+	
+	
 
 }
